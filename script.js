fb408e88f860f1ca79cd6535e7c8a299be65dd61
MackTay <191326787+MackTay@users.noreply.github.com>
2025-04-25 14:11:27 +0900

    Add Book constructor

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 /* JavaScript code goes here */
 
 const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must include 'new' to call the constuctor!");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
