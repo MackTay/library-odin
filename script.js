0e785309dc9cdaf2f1e0e79430f596cb69179732
MackTay <191326787+MackTay@users.noreply.github.com>
2025-04-28 13:15:36 +0900

    Add object to myLibrary, swap read for hotOrNot
    Want to be able to test javascript function to see if it's adding
    objects to the DOM tree and displaying properly. Add an example book
    object as the first book.
    
    Instead of read or not, have hot or not category. Change this in the JS.

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 /* JavaScript code goes here */
 
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Kafka on the Shore",
+        author: "Haruki Murakami",
+        pages: "480",
+        hotOrNot: "hot",
+        id: crypto.randomUUID
+    }
+];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, hotOrNot) {
     if (!new.target) {
         throw Error("You must include 'new' to call the constuctor!");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.hotOrNot = hotOrNot;
     this.id = crypto.randomUUID;
 }
 
-function addToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
+function addToLibrary(title, author, pages, hotOrNot) {
+    const book = new Book(title, author, pages, hotOrNot);
     return myLibrary.push(book);
 }
+
