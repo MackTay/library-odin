ff66b219895414f6ba09c84e9422e1dd3e037467
MackTay <191326787+MackTay@users.noreply.github.com>
2025-04-28 16:47:46 +0900

    Add table row back, write pseudocode for js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const myLibrary = [
         title: "Kafka on the Shore",
         author: "Haruki Murakami",
         pages: "480",
-        hotOrNot: "hot",
+        hotOrNot: "Hot",
         id: crypto.randomUUID
     }
 ];
@@ -29,6 +29,25 @@ function addToLibrary(title, author, pages, hotOrNot) {
 }
 
 const table = document.getElementById("table");
-const row = document.createElement("tr");
 const cell = document.createElement("td");
+
+/*
+Every time the function runs, we need to replace the table on the screen with a new one that uses data from mLibrary array. So... Step:
+1. remove the old table content
+2. For every book object in myLibrary:
+    - Append a new row to the tbody with an id
+    - Append a td for each data from the object to the row
+
+*/
+
+function addToTable(myLibrary) {
+    
+    const row = document.createElement("tr");
+    row.id = "row"
+
+    myLibrary.forEach(book => {
+        row.appendChild(cell)
+        .textContent = book;
+    });
+}
 
